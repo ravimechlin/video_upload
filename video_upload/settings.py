ec58171d9e040669fa5e4937c81d96bfdfee3baa
ravimechlin <ravi.kumar@mechlintech.com>
2023-10-15 01:03:04 +0530

    fixed code

--- video_upload/settings.py
+++ video_upload/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,8 +30,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 # Application definition
-import django
-django.setup()
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -42,9 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "import_export",
-    "app",
-    "waffle"
-
+    "app"
 ]
 
 MIDDLEWARE = [
@@ -55,9 +51,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'waffle.middleware.WaffleMiddleware',
-    "django.contrib.contenttypes.models.ContentType"
-    
 ]
 
 ROOT_URLCONF = "video_upload.urls"
@@ -84,23 +77,18 @@ WSGI_APPLICATION = "video_upload.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),         # Replace with your database name
-#         'USER': os.environ.get('DB_USER'),             # Replace with your database user
-#         'PASSWORD':os.environ.get('DB_PASSWORD'),     # Replace with your database password
-#         'HOST': 'db',                # This should match the name of the database service in your Docker Compose file
-#         'PORT': '5432',              # PostgreSQL default port
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),         # Replace with your database name
+        'USER': os.environ.get('DB_USER',),             # Replace with your database user
+        'PASSWORD':os.environ.get('DB_PASSWORD'),     # Replace with your database password
+        'HOST': 'db'  ,             # This should match the name of the database service in your Docker Compose file
+        'PORT': '5432',              # PostgreSQL default port
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
